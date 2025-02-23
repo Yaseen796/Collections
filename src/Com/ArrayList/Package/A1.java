6e3ef9b9ac0f5547efa2369c4d571589d6e9c385
Yaseen796 <shaikaman796@gmail.com>
2025-02-23 23:17:04 +0530

    New codes

--- src/Com/ArrayList/Package/A1.java
+++ src/Com/ArrayList/Package/A1.java
@@ -8,6 +8,13 @@ public class A1 {
         list.add(3); // 0
         list.add(2); // 1
         list.add(40); //2
+        list.add(12);
+        list.add(76);
+        list.add(22); // 0
+        list.add(8728); // 1
+        list.add(4021); //2
+        list.add(1112);
+        list.add(716);
 //        System.out.println(list.get(2));
 //        System.out.println(list.size());
 //
@@ -17,6 +24,7 @@ public class A1 {
         list.add(2,70);
         System.out.println(list);
 
+        System.out.println(list.size());
 
 
 //        for(int i = 0; i <list.size() ; i++) {
